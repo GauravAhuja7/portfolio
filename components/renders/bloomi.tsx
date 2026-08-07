5f3555b21c795657dd1963c11e22017c338d5760
Gaurav Ahuja <gauravahuja2244@gmail.com>
2026-08-07 21:51:32 +0530

    add project readmes

---
+++ components/renders/bloomi.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+export default function BloomiReadMe() {
+  return (
+    <div className="space-y-5">
+      <p>
+        As a college student living in a hostel, managing expenses is really
+        crucial to make sure you&apos;re not overspending on pizzas 🍕.
+      </p>
+      <p>
+        My friends and I always debate about who spends more money on junk food.
+        We keep promising to hit the gym regularly and stay away from junk, but
+        those promises never last.
+      </p>
+      <p>
+        I wanted to make expense tracking more fun and social — something where
+        you can track your spending with friends with leaderboard and ofc
+        without having to manually fill every field.
+      </p>
+      <p>
+        That&apos;s why I started building Bloomi — apps like YNAB or other
+        money managers aren&apos;t UI-friendly, and their UX didn&apos;t match
+        how I wanted to use an expense tracker.
+      </p>
+      <p>
+        I needed a simple expense tracker where you can capture an image and
+        instantly add an expense.
+      </p>
+      <p>I spent free time of my summar vacation working on this</p>
+      <p>
+        I&apos;m not actively working on Bloomi at the moment, but I&apos;ll
+        definitely be adding more features in the future.
+      </p>
+
+      <div>
+        <p>
+          site:{" "}
+          <Link
+            target="_blank"
+            href={"https://fixyourspend.vercel.app/"}
+            className="text-primary hover:underline"
+          >
+            bloomi.live
+          </Link>
+        </p>
+
+        <p>
+          repo:{" "}
+          <Link
+            target="_blank"
+            href={"https://github.com/sankalpaacharya/Bloomi/"}
+            className="text-primary hover:underline"
+          >
+            github.com/sankalpaacharya/bloomi
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
