dea7e681b82b44a178addc2961cfd85484af3ecd
Gaurav Ahuja <gauravahuja2244@gmail.com>
2026-08-07 22:05:57 +0530

    clear out projects folder

--- components/file-manager.tsx
+++ components/file-manager.tsx
@@ -8,14 +8,9 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { AboutMe } from "./renders/about-me";
-import WerideReadme from "./renders/weride";
-import BloomiReadMe from "./renders/bloomi";
 import Link from "next/link";
-import Chatcn from "./renders/chatcn";
-import ZeroTwoReadMe from "./renders/zerotwo";
 import { Experience } from "./renders/experience";
 import Reflection from "./renders/reflection";
-import Blogs from "./renders/blogs";
 import { Hobbies } from "./renders/hobbies";
 
 const data: FileNode[] = [
@@ -23,185 +18,6 @@ const data: FileNode[] = [
     type: "folder",
     name: "projects",
     children: [
-      {
-        type: "folder",
-        name: "chatcn",
-        children: [
-          {
-            type: "file",
-            name: "3d component.mp4",
-            thumbnail: "/images/chatcnvideo.png",
-            render: (
-              <div>
-                <iframe
-                  src="https://www.youtube.com/embed/DN1qRnZK-mw?si=HeXcE1zOjd1T22mx"
-                  title="YouTube video player"
-                  className="w-full h-40"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  referrerPolicy="strict-origin-when-cross-origin"
-                  allowFullScreen
-                ></iframe>
-              </div>
-            ),
-          },
-          {
-            type: "file",
-            name: "preview.png",
-            thumbnail: "/images/chatcn.png",
-            render: (
-              <div>
-                <img src="/images/chatcn.png" />
-              </div>
-            ),
-          },
-          {
-            type: "file",
-            name: "README.md",
-            render: <Chatcn />,
-          },
-          {
-            type: "file",
-            name: ".vscode",
-
-            src: "https://github.dev/sankalpaacharya/chatcn",
-          },
-        ],
-      },
-      {
-        type: "folder",
-        name: "bloomi",
-        children: [
-          {
-            type: "file",
-            name: "video.mp4",
-            thumbnail: "/images/bloomivideo.png",
-            render: (
-              <div>
-                <iframe
-                  className="w-full h-100"
-                  src="https://www.youtube.com/embed/SuxJXAgvEAo?si=cPMFJA3V9EP6TVXH"
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  referrerPolicy="strict-origin-when-cross-origin"
-                  allowFullScreen
-                ></iframe>
-              </div>
-            ),
-          },
-          {
-            type: "file",
-            name: "preview.png",
-            thumbnail: "/images/bloomiapp.png",
-            render: (
-              <div>
-                <img src="/images/bloomiapp.png" />
-              </div>
-            ),
-          },
-          {
-            type: "file",
-            name: "README.md",
-            render: <BloomiReadMe />,
-          },
-          {
-            type: "file",
-            name: ".vscode",
-
-            src: "https://github.dev/sankalpaacharya/bloomi",
-          },
-        ],
-      },
-      {
-        type: "folder",
-        name: "weride",
-        children: [
-          {
-            type: "file",
-            name: "weride.mp4",
-            thumbnail: "/images/weridevideo.png",
-            render: (
-              <div>
-                <iframe
-                  className="w-full h-100"
-                  src="https://www.youtube.com/embed/f2gtGsR3yHo?si=f0LOQQhvIqpUr36h"
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  referrerPolicy="strict-origin-when-cross-origin"
-                  allowFullScreen
-                ></iframe>
-              </div>
-            ),
-          },
-
-          {
-            type: "file",
-            name: "preview.png",
-            thumbnail: "https://www.sankalpa.info.np/images/weride.png",
-            render: (
-              <div>
-                <img src="/images/weride.png" />
-              </div>
-            ),
-          },
-
-          { type: "file", name: "README.md", render: <WerideReadme /> },
-          {
-            type: "file",
-            name: ".vscode",
-
-            src: "https://github.dev/sankalpaacharya/weride",
-          },
-        ],
-      },
-      {
-        type: "folder",
-        name: "zero two",
-        children: [
-          {
-            type: "file",
-            name: "zerotwo.mov",
-            thumbnail: "/images/zerotwovideo.png",
-            render: (
-              <div>
-                <iframe
-                  className="h-100 w-full"
-                  src="https://www.youtube.com/embed/mxJDRVZv_Z0?si=dqOIsAbtN6d1dVBh"
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  referrerPolicy="strict-origin-when-cross-origin"
-                  allowFullScreen
-                ></iframe>
-              </div>
-            ),
-          },
-          {
-            type: "file",
-            name: "preview.png",
-            thumbnail: "/images/zerotwo.png",
-            render: (
-              <div>
-                <img src="/images/zerotwo.png" />
-              </div>
-            ),
-          },
-
-          {
-            type: "file",
-            name: "README.md",
-            render: <ZeroTwoReadMe />,
-          },
-          {
-            type: "file",
-            name: ".vscode",
-
-            src: "https://github.dev/sankalpaacharya/zero-two",
-          },
-        ],
-      },
       {
         type: "file",
         name: "Reflection.md",
@@ -218,11 +34,6 @@ const data: FileNode[] = [
     type: "file",
     name: "Experience.md",
     render: <Experience />,
-  },
-  {
-    type: "file",
-    name: "Blogs.md",
-    render: <Blogs />,
   },
   {
     type: "file",
